d39510d314158a3113a5b393e8f106dc84bd7522
loso-nd <losond8@gmail.com>
2025-01-02 15:36:56 -0600

    useEffect, useCallback, Deleting items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import {BiArchive} from "react-icons/bi"
+import { useEffect, useCallback, useState } from "react";
 import Search from "./compontents/Search";
 import AddAppointment from "./compontents/AddAppointment";
 import AppointmentInfo from "./compontents/AppointmentInfo";
-import appointmentList from "./data.json"
+
+import {BiArchive} from "react-icons/bi"
 
 function App() {
+
+  const [appointmentList, setAppointmentList] = useState([]);
+
+  //fetching data can be done from a local file or a server
+  //retrieve the data and asking useCallback to track and changes that happen to that data
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+    .then(res => res.json())
+    .then(data => {
+      setAppointmentList(data)
+    });
+  }, []);
+
+
+// ask usedEffect to track any thing that happens to our fetchData
+  useEffect(() => {
+    fetchData();
+  }, [fetchData])
+
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
+      <h1 className="text-5xl  pb-3 ">
         <BiArchive  className="inline-block text-red-400 align-top"/> 
         Your Appointments
       </h1>
       <AddAppointment />
       <Search />
-      <AppointmentInfo  appointmentList={appointmentList}/>
+      <AppointmentInfo  appointmentList={appointmentList} 
+        onDeleteAppointment={appointmentId => setAppointmentList(appointmentList.filter(
+          appointment => appointment.id !== appointmentId
+        ))}/>
 
     </div>
   );
--- src/compontents/AppointmentInfo.js
+++ src/compontents/AppointmentInfo.js
@@ -2,13 +2,14 @@ import {BiTrash} from "react-icons/bi"
 
 import React from 'react'
 
-const AppointmentInfo = ({appointmentList}) => {
+const AppointmentInfo = ({appointmentList, onDeleteAppointment}) => {
   return (
     <div>
         <ul className="divide-y divide-gray-200">
         {appointmentList.map(appoinntment => (
           <li className="px-3 py-3 flex items-start">
             <button type="button"
+              onClick={() => onDeleteAppointment(appoinntment.id)}
               className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700
                   focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
               <BiTrash />
